7847bd21a424bae294ecb87d5c02d2e2c222e765
narek <narek@DESKTOP-98JS365>
2024-06-20 10:46:39 +0300

    Добавьте файлы проекта.

---
+++ 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 2, 9, 1 };
+        int maxNumber = FindMaxNumber(numbers);
+        Console.WriteLine($"Наибольшее значение в массиве: {maxNumber}");
+    }
+
+    static int FindMaxNumber(int[] array)
+    {
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
